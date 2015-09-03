ee283f577592a067ae4b2a51932ace91ddfac400
zhanghao <zh6335901@hotmail.com>
2015-09-03 13:39:33 +0800

    add ProductSearchRequest model

---
+++ src/ElasticSearchSample/Models/Product/ProductSearchRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElasticSearchSample.Models
+{
+    public class ProductSearchRequest
+    {
+        public string Keyword { get; set; }
+
+        public Dictionary<string, object> Terms { get; set; }
+        
+        public string SortField { get; set; }
+
+        public bool SortDescending { get; set; } = true;
+
+        public int From { get; set; } = 0;
+
+        public int Size { get; set; } = 10;
+    }
+}
